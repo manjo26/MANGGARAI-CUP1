1233f2f9de6ae021fe15d7d4c7c73149f3b3d19f
manjo26 <148962671+manjo26@users.noreply.github.com>
2026-03-08 17:19:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,87 @@
-let grupA = [
-"PALOPO FC",
-"ARENA FC",
-"ARSENAL",
-"AYAM GEPREK FC",
-"JURU KUNCI FC"
-]
+let data = JSON.parse(localStorage.getItem("klasemen"))
 
-let grupB = [
-"MANGGARAI RAYA",
-"BANGKA NACAP FC",
-"GAPURTO FC",
-"PSCEWANG",
-"PESONA FC"
-]
+if(!data){
 
-function buatTabel(){
+data = {
 
-let tabelA = document.getElementById("grupA")
-let tabelB = document.getElementById("grupB")
+"PALOPO FC":{m:0,w:0,d:0,l:0,p:0,g:"A"},
+"ARENA FC":{m:0,w:0,d:0,l:0,p:0,g:"A"},
+"ARSENAL":{m:0,w:0,d:0,l:0,p:0,g:"A"},
+"AYAM GEPREK FC":{m:0,w:0,d:0,l:0,p:0,g:"A"},
+"JURU KUNCI FC":{m:0,w:0,d:0,l:0,p:0,g:"A"},
 
-grupA.forEach((tim,index)=>{
+"MANGGARAI RAYA":{m:0,w:0,d:0,l:0,p:0,g:"B"},
+"BANGKA NACAP FC":{m:0,w:0,d:0,l:0,p:0,g:"B"},
+"GAPURTO FC":{m:0,w:0,d:0,l:0,p:0,g:"B"},
+"PSCEWANG":{m:0,w:0,d:0,l:0,p:0,g:"B"},
+"PESONA FC":{m:0,w:0,d:0,l:0,p:0,g:"B"}
 
-let row = `
+}
+
+}
+
+function simpan(){
+localStorage.setItem("klasemen",JSON.stringify(data))
+}
+
+function tampil(){
+
+let A=[]
+let B=[]
+
+for(let tim in data){
+
+if(data[tim].g==="A"){
+A.push([tim,data[tim]])
+}else{
+B.push([tim,data[tim]])
+}
+
+}
+
+A.sort((a,b)=>b[1].p-a[1].p)
+B.sort((a,b)=>b[1].p-a[1].p)
+
+let tabelA=document.getElementById("grupA")
+let tabelB=document.getElementById("grupB")
+
+if(!tabelA) return
+
+tabelA.innerHTML=""
+tabelB.innerHTML=""
+
+A.forEach((t,i)=>{
+
+tabelA.innerHTML+=`
 <tr>
-<td>${index+1}</td>
-<td>${tim}</td>
-<td>0</td>
-<td>0</td>
-<td>0</td>
-<td>0</td>
-<td>0</td>
+<td>${i+1}</td>
+<td>${t[0]}</td>
+<td>${t[1].m}</td>
+<td>${t[1].w}</td>
+<td>${t[1].d}</td>
+<td>${t[1].l}</td>
+<td>${t[1].p}</td>
 </tr>
 `
-
-tabelA.innerHTML += row
 
 })
 
-grupB.forEach((tim,index)=>{
+B.forEach((t,i)=>{
 
-let row = `
+tabelB.innerHTML+=`
 <tr>
-<td>${index+1}</td>
-<td>${tim}</td>
-<td>0</td>
-<td>0</td>
-<td>0</td>
-<td>0</td>
-<td>0</td>
+<td>${i+1}</td>
+<td>${t[0]}</td>
+<td>${t[1].m}</td>
+<td>${t[1].w}</td>
+<td>${t[1].d}</td>
+<td>${t[1].l}</td>
+<td>${t[1].p}</td>
 </tr>
 `
-
-tabelB.innerHTML += row
 
 })
 
 }
 
-buatTabel()
+tampil()
