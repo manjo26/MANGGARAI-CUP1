f71e8eea4e4e0e0224eca7b2aedb2eafa098a8bf
manjo26 <148962671+manjo26@users.noreply.github.com>
2026-03-08 17:09:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,91 +1,60 @@
-let klasemen = {
-"PALOPO FC":{m:0,w:0,d:0,l:0,p:0,g:"A"},
-"ARENA FC":{m:0,w:0,d:0,l:0,p:0,g:"A"},
-"ARSENAL":{m:0,w:0,d:0,l:0,p:0,g:"A"},
-"AYAM GEPREK FC":{m:0,w:0,d:0,l:0,p:0,g:"A"},
-"JURU KUNCI FC":{m:0,w:0,d:0,l:0,p:0,g:"A"},
+let grupA = [
+"PALOPO FC",
+"ARENA FC",
+"ARSENAL",
+"AYAM GEPREK FC",
+"JURU KUNCI FC"
+]
 
-"MANGGARAI RAYA":{m:0,w:0,d:0,l:0,p:0,g:"B"},
-"BANGKA NACAP FC":{m:0,w:0,d:0,l:0,p:0,g:"B"},
-"GAPURTO FC":{m:0,w:0,d:0,l:0,p:0,g:"B"},
-"PSCEWANG":{m:0,w:0,d:0,l:0,p:0,g:"B"},
-"PESONA FC":{m:0,w:0,d:0,l:0,p:0,g:"B"}
-};
+let grupB = [
+"MANGGARAI RAYA",
+"BANGKA NACAP FC",
+"GAPURTO FC",
+"PSCEWANG",
+"PESONA FC"
+]
 
-function tampil(){
+function buatTabel(){
 
-let A=[]
-let B=[]
+let tabelA = document.getElementById("grupA")
+let tabelB = document.getElementById("grupB")
 
-for(let tim in klasemen){
+grupA.forEach((tim,index)=>{
 
-if(klasemen[tim].g==="A"){
-A.push({nama:tim,...klasemen[tim]})
-}else{
-B.push({nama:tim,...klasemen[tim]})
-}
-
-}
-
-A.sort((a,b)=>b.p-a.p)
-B.sort((a,b)=>b.p-a.p)
-
-let tabelA=document.getElementById("grupA")
-let tabelB=document.getElementById("grupB")
-
-tabelA.innerHTML=""
-tabelB.innerHTML=""
-
-tabelA.innerHTML=`
+let row = `
 <tr>
-<th>No</th>
-<th>Tim</th>
-<th>M</th>
-<th>W</th>
-<th>D</th>
-<th>L</th>
-<th>P</th>
+<td>${index+1}</td>
+<td>${tim}</td>
+<td>0</td>
+<td>0</td>
+<td>0</td>
+<td>0</td>
+<td>0</td>
 </tr>
 `
 
-tabelB.innerHTML=`
-<tr>
-<th>No</th>
-<th>Tim</th>
-<th>M</th>
-<th>W</th>
-<th>D</th>
-<th>L</th>
-<th>P</th>
-</tr>
-`
+tabelA.innerHTML += row
 
-A.forEach((t,i)=>{
-tabelA.innerHTML+=`
-<tr>
-<td>${i+1}</td>
-<td>${t.nama}</td>
-<td>${t.m}</td>
-<td>${t.w}</td>
-<td>${t.d}</td>
-<td>${t.l}</td>
-<td>${t.p}</td>
-</tr>`
 })
 
-B.forEach((t,i)=>{
-tabelB.innerHTML+=`
+grupB.forEach((tim,index)=>{
+
+let row = `
 <tr>
-<td>${i+1}</td>
-<td>${t.nama}</td>
-<td>${t.m}</td>
-<td>${t.w}</td>
-<td>${t.d}</td>
-<td>${t.l}</td>
-<td>${t.p}</td>
-</tr>`
+<td>${index+1}</td>
+<td>${tim}</td>
+<td>0</td>
+<td>0</td>
+<td>0</td>
+<td>0</td>
+<td>0</td>
+</tr>
+`
+
+tabelB.innerHTML += row
+
 })
 
 }
 
-tampil()
+buatTabel()
